a7d5c27069e3320251cf41186d92e50ea8ba447c
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-07-01 05:04:12 +0100

    Method that determines if a given data set represents a valid UTF-8 encoding.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    """
+    Checks if the given byte sequence represents a valid UTF-8 encoding.
+    Note: Each integer represents 1 byte of data, therefore you only
+    need to handle the 8 least significant bits of each integer
+
+    Args:
+        data: A list of integers representing bytes.
+
+    Returns:
+        True if the data is a valid UTF-8 encoding, False otherwise.
+    """
+
+    for num in data:
+        bin_rep = format(num, '08b')
+        if bin_rep.startswith('0') or\
+                bin_rep.startswith('110') or\
+                bin_rep.startswith('1110') or\
+                bin_rep.startswith('11110'):
+            continue
+        else:
+            return False
+    return True
